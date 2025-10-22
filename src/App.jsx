a005a5896170f3c733203cfa7a260f7174556dab
jigyasha1234 <jigrani0000@gmail.com>
2025-10-22 19:56:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import LandingPage from './pages/landingPage'
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <LandingPage />
+    </div>
+  )
+}
